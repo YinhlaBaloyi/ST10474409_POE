07305832a72444656e4c9f1353b0e4acc9b91d6f
YinhlaBaloyi <st10474409@rcconnect.edu.za>
2025-06-20 19:22:38 +0200

    changes

--- form.js
+++
@@ -1,8 +0,0 @@
-    document.getElementById("application").onclick = function(){
-            alert('Thank you for your application. We will send you an email with a feedback')
-        };
-    document.getElementById("contact").onclick = function(){
-            alert('Thank you for contacting us, We will get back to soon!!')
-        };
-
-  
